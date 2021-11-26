e81c16b45d1fd6686b9ef816aa161f48589206f8
1uLang <243971996@qq.com>
2021-11-26 21:03:49 +0800

    authority plus bug

--- internal/nodes/api_node_services_plus.go
+++ internal/nodes/api_node_services_plus.go
@@ -4,16 +4,13 @@
 package nodes
 
 import (
-	"github.com/TeaOSLab/EdgeAPI/internal/rpc/services"
-	"github.com/TeaOSLab/EdgeCommon/pkg/rpc/pb"
 	"google.golang.org/grpc"
 )
 
 func APIAuthorityKeyServicesRegister(node *APINode, server *grpc.Server) {
-	{
-		instance := node.serviceInstance(&services.AuthorityKeyService{}).(*services.AuthorityKeyService)
-		pb.RegisterAuthorityKeyServiceServer(server, instance)
-		node.rest(instance)
-	}
+	//{
+	//	instance := node.serviceInstance(&services.AuthorityKeyService{}).(*services.AuthorityKeyService)
+	//	pb.RegisterAuthorityKeyServiceServer(server, instance)
+	//	node.rest(instance)
+	//}
 }
-
